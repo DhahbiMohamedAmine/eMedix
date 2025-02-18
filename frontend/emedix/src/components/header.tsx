6166f1557d9f0f543c676b66c14433c5fe776027
AmenZlul <amenallah0benali@gmail.com>
2025-02-18 16:13:43 +0100

    added footer/header/profile/medicaments management /list of users COMPONENETS

---
+++ frontend/emedix/src/components/header.tsx
@@ -0,0 +1,45 @@
+import "../../public/tailwind.css"
+import { useState } from "react"
+import Link from "next/link"
+
+
+export default function HeaderComponent() {
+
+
+    return (
+       
+          <header className="bg-blue-500 text-white px-4 py-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64">
+            <div className="flex justify-between items-center">
+              <Link href="/home" className="text-white font-bold text-2xl">
+                eMedix
+              </Link>
+              <nav>
+                <ul className="flex items-center">
+                  <li className="ml-6">
+                    <Link href="/home" className="text-white hover:text-blue-200">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="ml-6">
+                    <Link href="/medicaments" className="text-white hover:text-blue-200">
+                      Medicaments
+                    </Link>
+                  </li>
+                  <li className="ml-6">
+                    <Link href="/profile" className="text-white hover:text-blue-200">
+                      Profile
+                    </Link></li>
+                    <li className="ml-6">
+                    <Link href="/userList" className="text-white hover:text-blue-200">
+                      Users List
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </header>
+       
+    )
+
+
+}
---
+++ frontend/emedix/src/components/ui/icons.tsx
@@ -0,0 +1,27 @@
+export const PlusIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+    </svg>
+  )
+  export const Trash2Icon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+      />
+    </svg>
+  )
+  export const PencilIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15.232 5.232l9.414 9.414a2 2 0 010 2.828l-6.364 6.364a2 2 0 01-2.828 0l-6.364-6.364a2 2 0 010-2.828l9.414-9.414zM12 11l.01.01M8 11a4 4 0 118 0 4 4 0 01-8 0z"
+      />
+    </svg>
+  )
+  
+  
